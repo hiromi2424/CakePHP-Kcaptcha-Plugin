4d1038e8e82aada20182915fd816ab41270e2088
hiromi2424 <tanaka2424@gmail.com>
2011-02-20 19:57:27 +0900

    improves Captchable Behavior more optional and powerful. added test.

--- models/behaviors/captchable.php
+++ models/behaviors/captchable.php
@@ -1,31 +1,73 @@
 <?php
 
 class CaptchableBehavior extends ModelBehavior {
-	var $answerProperty = 'captchaAnswer';
-	var $field = 'captcha';
-	var $rule = 'captcha';
+
+	var $settings = array();
+
+	var $defaultOptions = array(
+		'answerProperty' => 'captchaAnswer',
+		'field' => 'captcha',
+		'rule' => 'captcha',
+		'convertKana' => true,
+		'trim' => '[\s\p{Zs}\p{Zl}\p{Zp}]',
+		'required' => true,
+		'setupValidation' => true,
+	);
+
+	var $answer;
 
 	function setup(&$model, $config = array()) {
-		$this->_set($config);
+		$this->settings[$model->alias] = array_merge($this->defaultOptions, $config);
+		if ($this->settings[$model->alias]['setupValidation']) {
+			$this->setupCaptchaValidation($model);
+		}
+	}
+
+	function setupCaptchaValidation(&$model) {
+		$field = $this->settings[$model->alias]['field'];
+		$rule = $this->settings[$model->alias]['rule'];
+
+		if (!isset($model->validate[$field][$rule])) {
+			$model->validate[$field][$rule] = array();
+		}
+		$model->validate[$field][$rule] = Set::merge($model->validate[$field][$rule], array(
+			'rule' => array('validCaptcha'),
+		));
+
+		$this->requireCaptcha($model, $this->settings[$model->alias]['required']);
 	}
 
 	function setCaptchaAnswer(&$model, $answer) {
-		$model->{$this->answerProperty} = $answer;
+		$model->{$this->settings[$model->alias]['answerProperty']} = $answer;
 	}
 
 	function requireCaptcha(&$model, $yes = true) {
-		$model->validate[$this->field][$this->rule]['required'] = $yes;
-		$model->validate[$this->field][$this->rule]['allowEmpty'] = !$yes;
+		$field = $this->settings[$model->alias]['field'];
+		$rule = $this->settings[$model->alias]['rule'];
+
+		$model->validate[$field][$rule]['required'] = !!$yes;
+		$model->validate[$field][$rule]['allowEmpty'] = !$yes;
 	}
 
 	function validCaptcha(&$model, $data) {
-		$check = current($data);
-		$check = mb_convert_kana($check, 'a');
+		$check = current((array)$data);
+		if ($this->settings[$model->alias]['convertKana']) {
+			$check = mb_convert_kana($check, 'a');
+		}
+		if ($this->settings[$model->alias]['trim']) {
+			$check = $this->_multibyteTrim($check, $this->settings[$model->alias]['trim']);
+		}
 
-		if (empty($model->{$this->answerProperty})) {
+		if (empty($model->{$this->settings[$model->alias]['answerProperty']})) {
 			return false;
 		}
-		return $check === $model->{$this->answerProperty};
+		return $check === $model->{$this->settings[$model->alias]['answerProperty']};
 	}
+
+	function _multibyteTrim($check, $space) {
+		$space = $space === true ? $this->defaultOptions['trim'] : $space;
+		return preg_replace("/(^$space+|$space+$)/mu", '', $check);
+	}
+
 }
 
---
+++ tests/cases/models/behaviors/captchable.test.php
@@ -0,0 +1,146 @@
+<?php
+/* Captchable Test cases generated on: 2010-11-24 13:11:13 : 1290571993*/
+
+class CaptchableBehaviorMockModel extends CakeTestModel {
+	var $useTable = false;
+}
+
+class CaptchableBehaviorTestCase extends CakeTestCase {
+	function startTest() {
+		$this->Model =& ClassRegistry::init('CaptchableBehaviorMockModel');
+		$this->Model->Behaviors->attach('Kcaptcha.Captchable');
+	}
+
+	function endTest() {
+		unset($this->Model);
+		ClassRegistry::flush();
+	}
+
+	function _reset($config = array()) {
+		$this->endTest();
+		$this->Model =& ClassRegistry::init('CaptchableBehaviorMockModel');
+		$this->Model->Behaviors->attach('Kcaptcha.Captchable', $config);
+	}
+
+	function testSetupCaptchaValidation() {
+		$expected = array(
+			'captcha' => array(
+				'captcha' => array(
+					'rule' => array('validCaptcha'),
+					'required' => true,
+					'allowEmpty' => false,
+				),
+			),
+		);
+		$this->assertEqual($expected, $this->Model->validate);
+
+		$this->_reset(array('setupValidation' => false));
+		$this->Model->validate = array(
+			'captcha' => array(
+				'notempty' => array(
+					'rule' => array('notempty'),
+					'required' => true,
+					'allowEmpty' => false,
+					'last' => true,
+				),
+				'captcha' => array(
+					'on' => 'update',
+				),
+			),
+		);
+		$this->Model->setupCaptchaValidation();
+		$expected = array(
+			'captcha' => array(
+				'notempty' => array(
+					'rule' => array('notempty'),
+					'required' => true,
+					'allowEmpty' => false,
+					'last' => true,
+				),
+				'captcha' => array(
+					'on' => 'update',
+					'rule' => array('validCaptcha'),
+					'required' => true,
+					'allowEmpty' => false,
+				),
+			),
+		);
+		$this->assertEqual($expected, $this->Model->validate);
+
+		$this->_reset(array('setupValidation' => false));
+		$this->Model->validate = array(
+			'captcha' => array(
+				'notempty' => array(
+					'rule' => array('notempty'),
+					'required' => true,
+					'allowEmpty' => false,
+					'last' => true,
+				),
+			),
+		);
+		$this->Model->setupCaptchaValidation();
+		$expected = array(
+			'captcha' => array(
+				'notempty' => array(
+					'rule' => array('notempty'),
+					'required' => true,
+					'allowEmpty' => false,
+					'last' => true,
+				),
+				'captcha' => array(
+					'rule' => array('validCaptcha'),
+					'required' => true,
+					'allowEmpty' => false,
+				),
+			),
+		);
+		$this->assertEqual($expected, $this->Model->validate);
+	}
+
+	function testSetCaptchaAnswer() {
+		$this->Model->setCaptchaAnswer('hogehoge');
+		$this->assertTrue(isset($this->Model->captchaAnswer));
+		$this->assertEqual('hogehoge', $this->Model->captchaAnswer);
+	}
+
+	function testRequireCaptcha() {
+		$this->Model->validate = array();
+		$this->Model->requireCaptcha();
+		$expected = array(
+			'captcha' => array(
+				'captcha' => array(
+					'required' => true,
+					'allowEmpty' => false,
+				),
+			),
+		);
+		$this->assertEqual($expected, $this->Model->validate);
+
+		$this->Model->requireCaptcha(false);
+		$expected = array(
+			'captcha' => array(
+				'captcha' => array(
+					'required' => false,
+					'allowEmpty' => true,
+				),
+			),
+		);
+		$this->assertEqual($expected, $this->Model->validate);
+	}
+
+	function testValidCaptcha() {
+		$this->assertFalse($this->Model->validCaptcha('was not set'));
+
+		$this->Model->setCaptchaAnswer('hogehoge');
+		$this->assertFalse($this->Model->validCaptcha('invalid'));
+		$this->assertTrue($this->Model->validCaptcha('hogehoge'));
+
+		$this->assertTrue($this->Model->validCaptcha('ｈｏｇｅｈｏｇｅ'));
+		$this->assertTrue($this->Model->validCaptcha('　ｈｏｇｅｈｏｇｅ '));
+		$this->Model->setCaptchaAnswer('1h2o3g4e');
+		$this->assertTrue('　1ｈ2o３ｇ４e');
+		$this->assertTrue($this->Model->validCaptcha('　1ｈ2o３ｇ４e'));
+	}
+
+}
+
